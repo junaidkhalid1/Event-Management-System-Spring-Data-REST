3d36a095ea6e3cfc9255089ccfb8f775cb894c45
Junaid KHALID <junaidkhalid1@aol.com>
2018-01-04 21:48:41 +0100

    *Custom Finder Methods implemented on Event

--- src/main/java/fr/junaid/eventmanagement/repos/EventRepository.java
+++ src/main/java/fr/junaid/eventmanagement/repos/EventRepository.java
@@ -1,11 +1,21 @@
 package fr.junaid.eventmanagement.repos;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import fr.junaid.eventmanagement.entities.Event;
+import java.lang.String;
+import java.time.ZoneId;
+import java.util.List;
 
 //public interface EventRepository extends CrudRepository<Event, Long> {
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
+	
+	// List<Event> findByName(@Param("name") String name); // Custome finder method to search Event by Name
+	Page<Event> findByName(@Param("name") String name, Pageable pageable); // Paging Support added
 
+	Page<Event> findByNameAndZoneId(@Param("name") String name, @Param("zoneId") ZoneId zoneId, Pageable pageable);// Find By multiple Parameters
 }
